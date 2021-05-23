e496048d3425b1d3f8e85abbe4748d0199169b42
Julian Vuoso <juvuoso@itba.edu.ar>
2021-05-23 12:59:53 -0300

    new pow & operations with table

--- include/galois.h
+++ include/galois.h
@@ -5,10 +5,13 @@
 #ifndef STENOGRAPHY_SHARED_SECRET_GALOIS_H
 #define STENOGRAPHY_SHARED_SECRET_GALOIS_H
 
+uint8_t galois_init();
 uint8_t galois_add(uint8_t v1, uint8_t v2);       /* Substraction between two elements of GF(2^8) */
 uint8_t galois_sub(uint8_t v1, uint8_t v2);       /* Add two elements of GF(2^8) */
 uint8_t galois_mul(uint8_t v1, uint8_t v2);       /* Multiply two elements of GF(2^8) */
 uint8_t galois_div(uint8_t v1, uint8_t v2);
+uint8_t galois_mul2(uint8_t v1, uint8_t v2);       /* Multiply two elements of GF(2^8) */
+uint8_t galois_div2(uint8_t v1, uint8_t v2);
 uint8_t galois_pow(uint8_t v, uint8_t e);
 void galois_print(uint8_t v);                   /* Print an element of GF(2^8) in binary form */
 
--- src/galois.c
+++ src/galois.c
@@ -5,10 +5,15 @@
  * The elements of GF(2^8) thus represent polynomials of degree < 8 in the generator x. Addition in this field is simply
  * bitwise XOR, but multiplication requires the elimination of powers of x <= 8.
  */
+
 #include <stdio.h>
 #include <stdint.h>
 #include <math.h>
 #include "../include/galois_lib.h"
+
+uint8_t galois_init(){
+    return galois_create_mult_tables(8);   /* Returns 0 on success, -1 on failure */
+}
 
 uint8_t galois_add(uint8_t v1, uint8_t v2){
     return v1 ^ v2;
@@ -19,15 +24,19 @@ uint8_t galois_sub(uint8_t v1, uint8_t v2){
 }
 
 uint8_t galois_mul(uint8_t v1, uint8_t v2){
-    return galois_single_multiply(v1,v2,8);
+    return galois_multtable_multiply(v1,v2,8);
 }
 
 uint8_t galois_div(uint8_t v1, uint8_t v2){
-    return galois_single_divide(v1,v2,8);
+    return galois_multtable_divide(v1,v2,8);
 }
 
 uint8_t galois_pow(uint8_t v, uint8_t e){
-    return (uint8_t) pow(v, e);
+    if(e==0) return 1;
+    uint8_t result = v;
+    for(int i = 1; i < e; i++)
+        result = galois_mul(result,v);
+    return result;
 }
 
 void galois_print(uint8_t v){
@@ -36,5 +45,3 @@ void galois_print(uint8_t v){
         putchar((v >> i & 1) + '0');
     putchar('\n');
 }
-
-
